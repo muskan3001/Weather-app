9847930043d2a4d465c421657c4694065786abc5
muskan3001 <mg8203@srmist.edu.in>
2021-07-22 22:37:43 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,44 @@
 const path = require("path");
-
 const express = require("express");
-app.set('view engine','hbs')
-const pbpath = path.join(__dirname, "../public");
+const hbs = require("hbs");
 const app = express();
-app.use(express.static(pbpath));
 
-app.get("/weather", (req, res) => {
-  res.send({
-    forecast: "it is rainy today",
-    location: "mumbai",
+const pbpath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(pbpath));
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "weather app",
+    name: "musssuuu",
   });
 });
-
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "this is about section",
+    name: "suarOP",
+  });
+});
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "this is some useful text",
+    title: "this is about section",
+    name: "suarOP",
+  });
+});
+app.get("/weather", (req, res) => {
+  res.render("help", {
+    title: "help section",
+    name: "help others",
+  });
+});
+app.get("*", (req, res) => {
+  res.send("my 404 page");
+});
 app.listen(3000, () => {
   console.log("server is up on port 3000");
 });
